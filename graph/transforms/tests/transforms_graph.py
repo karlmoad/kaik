5aee452ae6b4202eb146dd7245c3847e9a18f6f3
Karl <karl.moad@gmail.com>
2024-10-15 14:49:58 -0400

    dev

--- graph/transforms/tests/transforms_graph.py
+++ graph/transforms/tests/transforms_graph.py
@@ -1,6 +1,24 @@
-from graph.transforms.tests import _init_test_data
 from graph.transforms.core import InputMappingTransform, ForceBidirectional
+from common.utils.test_utils import load_test_dataframe_from_csv
 
+def _init_test_data():
+    data = {
+        "nodes": load_test_dataframe_from_csv('nodes.csv', sep="|"),
+        "edges": load_test_dataframe_from_csv('edges.csv', sep="|")
+    }
+    
+    mapping = {
+        'edges': {'source': 'ORIGIN', 'target': 'TARGET', 'graph_id': 'GRAPH', 'edge_type': 'TYPE', 'class': 'CLASS',
+                  'features': 'FEATURES'},
+        'nodes': {'label': 'LABEL', 'node_type': 'TYPE', 'features': 'FEATURES', "class": "CLASS"},
+    }
+    
+    defaults = {
+        'nodes': {"node_type": "NODE", "features": "-NONE-", "class": "NODE"},
+        'edges': {"edge_type": "EDGE", "graph_id": 0, "features": "-NONE-", "class": "EDGE"},
+    }
+    
+    return data, mapping, defaults
 
 def test_input_mapping():
     data, mapping,defaults = _init_test_data()
